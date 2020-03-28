ae85b63b84a1bcd870f8f770e087fb37cf72e5ff
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-28 13:38:50 -0300

    add authentication to sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,10 @@ Rails.application.routes.draw do
   get 'global/deaths', to: 'covid_info_global#deaths'
   get 'global/daily', to: 'covid_info_global#daily'
   
-  mount Sidekiq::Web => '/sidekiq'
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end if Rails.env.production?
+  mount Sidekiq::Web, at: "/sidekiq"
 end
