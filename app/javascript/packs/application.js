f08cc45084df4923982db74dea450a922b66faaa
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-03-29 19:23:36 -0300

    add notification js alert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+global.toastr = require("toastr")
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -9,6 +10,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
 require("popper.js")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
